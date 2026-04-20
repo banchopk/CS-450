2019fab539809030fcf1b834f58239bcffd6a55e
banchopk <164674587+banchopk@users.noreply.github.com>
2026-04-19 21:59:52 -0700

    Initial commit

---
+++ src/mod1-1.cpp
@@ -0,0 +1,20 @@
+/* Simple Program with a few Errors for Correction */
+
+#include <iostream>
+
+// Standard namespace declaration
+using namespace std;
+
+// Main Function
+int main()
+{
+    // Standard Output Statement
+    cout << "Welcome to C++ Program" << endl;
+    cout << "I have corrected all errors for this program." << endl;
+
+    // Wait for output screen (optional)
+    cin.get();
+
+    // Main Function return statement
+    return 0;
+}
---
+++ src/simple.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string firstName = "Bradd";
+    string lastName = "Pitted";
+    string streetAddress = "676 Elm Street";
+    string city = "Beverly Hills";
+    string zipCode = "90210";
+
+    cout << "First Name: " << firstName << endl;
+    cout << "Last Name: " << lastName << endl;
+    cout << "Street Address: " << streetAddress << endl;
+    cout << "City: " << city << endl;
+    cout << "Zip Code: " << zipCode << endl;
+
+    return 0;
+}
